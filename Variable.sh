146548617d956d1092af9ab22121639c2327083f
venatisaipavan <venatisaipavan@gmail.com>
2026-02-10 11:31:25 +0530

    person2 added

---
+++ Variable.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+Person1=Sai
+Person2=Pavan
+
+echo "$PERSON1:: Hello $PERSON2,how are you? "
+echo "$PERSON2:: Hey Buddy,I am good..How are you doing ?"
+echo "$PERSON1:: I am doing good $PERSON2,what going on "
+echo "$PERSON2:: Nothing $PERSON1, All is going okay"
+echo "$PERSON1:: Oh Okay $PERSON2, I have to go..can we meet tomorrow for a quick chat?"
+echo "$PERSON2:: $PERSON1, I am a bit busy tomorrow, can we connect day after tomorrow ?"
