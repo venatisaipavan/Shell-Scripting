91fb96b977e6ebf09ce87cec9c871624b3f5f8f8
venatisaipavan <venatisaipavan@gmail.com>
2026-02-10 11:22:31 +0530

    new files2

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+echo "Suresh:: Hello Ramesh,how are you? "
+echo "Ramesh:: Hey Buddy,I am good..How are you doing ?"
+echo "Suresh:: I am doing good Ramesh,what going on "
+echo "Ramesh:: Nothing Suresh, All is going okay"
+echo "Suresh:: Oh Okay Ramesh, I have to go..can we meet tomorrow for a quick chat?"
+echo "Ramesh:: Suresh, I am a bit busy tomorrow, can we connect day after tomorrow ?"
