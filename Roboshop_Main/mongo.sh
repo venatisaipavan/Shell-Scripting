c49bd32df4eeee87d4a9c70fe8a0c73914e77182
venatisaipavan <venatisaipavan@gmail.com>
2026-03-04 10:15:45 +0530

    mongodb.sh updated

--- Roboshop_Main/mongo.sh
+++ Roboshop_Main/mongo.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 LOG_DIR="/var/log/roboshop/"
 LOG_Path="LOG_DIR/$0.log"
 
 if [ $ID -ne 0 ]; then
-    echo " Script Need Root Privilages..Exiting!" | tee -a $LOG_Path
+    echo "$R Script Need Root Privilages..Exiting! $N" | tee -a $LOG_Path
     exit 1
     else
- echo "Thanks for running as Root" | tee -a $LOG_Path
+ echo "$G Thanks for running as Root $N " | tee -a $LOG_Path
 fi
 
 VALIDATE() {
    if [ $? -ne 0]; then
-    echo "$2... failure" &>>$LOG_Path
+    echo "$R$2... failure $N" &>>$LOG_Path
     else
-    echo "$2... success" &>>$LOG_Path
+    echo " $G$2... success $N" &>>$LOG_Path
    fi
 }
 
 mkdir -p $LOG_DIR
+
+rm -rf  /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Remove Old mongo repo"
 
 cp mongo.repo /etc/yum.repos.d/ &>>$LOG_Path
 VALIDATE $? "Copy mongo repo"
