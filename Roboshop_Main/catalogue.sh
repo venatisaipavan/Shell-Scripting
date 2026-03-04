8bb548a969b6d7c9f23806d2ac1bca9ceabd6e5a
venatisaipavan <venatisaipavan@gmail.com>
2026-03-04 18:18:07 +0530

    mongodb.sh updated

--- Roboshop_Main/catalogue.sh
+++ Roboshop_Main/catalogue.sh
@@ -11,64 +11,66 @@ LOGS_DIR="/var/log/Roboshop/"
 LOGS_PATH="$LOGS_DIR/$0.log"
 
 if [ $ID -ne 0 ]; then
-echo -e "$R This need Root Privilages..Exiting $N" | tee -a $LOGS_DIR
+echo -e "$R This need Root Privilages..Exiting $N" | tee -a $LOGS_PATH
 exit 1
 else
-echo -e "$G Thanks for Running via Root..$N " | tee -a $LOGS_DIR
+echo -e "$G Thanks for Running via Root..$N " | tee -a $LOGS_PATH
+fi
 
 mkdir -p $LOGS_DIR
 
 VALIDATE(){
     if [ $? -ne 0 ]; then
-    echo -e "$R$2 is failure... $N" &>>$LOGS_DIR
+    echo -e "$R$2 is failure... $N" | tee -a $LOGS_PATH
     else
-    echo -e "$G$2 is Success... $N" &>>$LOGS_DIR
+    echo -e "$G$2 is Success... $N" | tee -a $LOGS_PATH
+    fi
 }
 
-dnf module disable nodejs -y  &>>$LOGS_DIR
+dnf module disable nodejs -y  &>>$LOGS_PATH
 VALIDATE $? "Disable Nodejs"
  
-dnf module enable nodejs:20 -y  &>>$LOGS_DIR
+dnf module enable nodejs:20 -y  &>>$LOGS_PATH
 VALIDATE $? "enable Nodejs20"
 
-dnf install nodejs -y  &>>$LOGS_DIR
+dnf install nodejs -y  &>>$LOGS_PATH
 VALIDATE $? "install Nodejs"
  
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOGS_DIR
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop  &>>$LOGS_PATH
 VALIDATE $? "Roboshop User creation"
 
-mkdir -p /app  &>>$LOGS_DIR
+mkdir -p /app  &>>$LOGS_PATH
 VALIDATE $? "crate /app directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip ; cd /app ; unzip /tmp/catalogue.zip &>>$LOGS_DIR
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip ; cd /app ; unzip /tmp/catalogue.zip &>>$LOGS_PATH
 VALIDATE $? "Copy and unzip catalogue to /app"
 
-npm install &>>$LOGS_DIR
+npm install &>>$LOGS_PATH
 VALIDATE $? "Dependency install"
 
 cd $DIR
 VALIDATE $? "change back to $DIR"
 
-rm -rf /etc/systemd/system/catalogue.service &>>$LOGS_DIR
+rm -rf /etc/systemd/system/catalogue.service &>>$LOGS_PATH
 VALIDATE $? "Remove old catalogue.service"
 
-cp catalogue.service /etc/systemd/system/ &>>$LOGS_DIR
+cp catalogue.service /etc/systemd/system/ &>>$LOGS_PATH
 VALIDATE $? "Copy catalogue service"
 
-systemctl daemon-reload &>>$LOGS_DIR
+systemctl daemon-reload &>>$LOGS_PATH
 VALIDATE $? "Reload systemd"
 
-systemctl enable --now catalogue  &>>$LOGS_DIR
+systemctl enable --now catalogue  &>>$LOGS_PATH
 VALIDATE $? "start and enable catalogue"
 
-rm -rf /etc/yum.repos.d/mongo.repo &>>$LOGS_DIR
+rm -rf /etc/yum.repos.d/mongo.repo &>>$LOGS_PATH
 VALIDATE $? "Remove mongo repo old"
 
-cp mongo.repo /etc/yum.repos.d/ &>>$LOGS_DIR
+cp mongo.repo /etc/yum.repos.d/ &>>$LOGS_PATH
 VALIDATE $? "Copy new mongo repo"
 
-dnf install mongodb-mongosh -y &>>$LOGS_DIR
+dnf install mongodb-mongosh -y &>>$LOGS_PATH
 VALIDATE $? "install mongodb-mongosh"
 
-mongosh --host mongodb.vsp-97.online </app/db/master-data.js &>>$LOGS_DIR
+mongosh --host mongodb.vsp-97.online </app/db/master-data.js &>>$LOGS_PATH
 VALIDATE $? "Copy js"
